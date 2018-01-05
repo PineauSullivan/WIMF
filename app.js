79972969bf579923cc5cc94df0efd4aed3de29b5
PineauSullivan <pineau.sullivan@gmail.com>
2018-01-05 17:01:14 +0100

    Master: fix generate

--- app.js
+++ app.js
@@ -9,28 +9,30 @@ function guid() {
 	s4() + '-' + s4() + s4() + s4();
 }
 function create(nForm){
-  	var channel = nForm.channel.value;
-	console.log('channel: ',channel);
+	if(nForm.channel.value!=""){
+	  	var channel = nForm.channel.value;
+		console.log('channel: ',channel);
 
-  	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function($evt){
-	if(xhr.readyState == 4 && xhr.status == 200){
-	     var res = JSON.parse(xhr.responseText);
-	     console.log('response: ',res);
-	     if(res.s=="ok"){
-		    document.getElementById("buttonCreate").style.backgroundColor = "green";
-	     	document.location.href="app/index.html?"+channel;
-	     }else{
-		    document.getElementById("buttonCreate").style.backgroundColor = "red";
-	     }
+	  	var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function($evt){
+		if(xhr.readyState == 4 && xhr.status == 200){
+		     var res = JSON.parse(xhr.responseText);
+		     console.log('response: ',res);
+		     if(res.s=="ok"){
+			    document.getElementById("buttonCreate").style.backgroundColor = "green";
+		     	document.location.href="app/index.html?"+channel;
+		     }else{
+			    document.getElementById("buttonCreate").style.backgroundColor = "red";
+		     }
+		}
+
+
+		xhr.open('PUT', "https://ws.xirsys.com/_ns/www.wimf.com/wimf-foglet/"+channel, true);
+		xhr.setRequestHeader ("Authorization", "Basic " + btoa("WhereIsMyFriends:3fea7eb8-d07b-11e7-be18-fd777e1dd627") );
+		xhr.send();
+
+	  }
 	}
-
-
-	xhr.open('PUT', "https://ws.xirsys.com/_ns/www.wimf.com/wimf-foglet/"+channel, true);
-	xhr.setRequestHeader ("Authorization", "Basic " + btoa("WhereIsMyFriends:3fea7eb8-d07b-11e7-be18-fd777e1dd627") );
-	xhr.send();
-
-  }
 
 }
 
@@ -44,6 +46,6 @@ function join(nForm){
 	}
 }
 
-function generate(nform){
+function generate(nForm){
 	nForm.channel.value = guid();
 }
