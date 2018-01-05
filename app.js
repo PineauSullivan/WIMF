4b8f688b50b44d2e8ec35e5da2d1d0fe1b8facae
PineauSullivan <pineau.sullivan@gmail.com>
2018-01-05 16:54:06 +0100

    Master : creation channel random

--- app.js
+++ app.js
@@ -8,30 +8,25 @@ function guid() {
 	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + 
 	s4() + '-' + s4() + s4() + s4();
 }
-
-var channelG =  guid();
-
 function create(nForm){
-	console.log('channel: ',channelG);
+  	var channel = nForm.channel.value;
+	console.log('channel: ',channel);
+
   	var xhr = new XMLHttpRequest();
-	console.log('Log 1');
 	xhr.onreadystatechange = function($evt){
 	if(xhr.readyState == 4 && xhr.status == 200){
-		console.log('Log 2');
 	     var res = JSON.parse(xhr.responseText);
 	     console.log('response: ',res);
 	     if(res.s=="ok"){
 		    document.getElementById("buttonCreate").style.backgroundColor = "green";
-	     	document.location.href="app/index.html?"+channelG;
+	     	document.location.href="app/index.html?"+channel;
 	     }else{
-		    channelG =  guid();
 		    document.getElementById("buttonCreate").style.backgroundColor = "red";
 	     }
 	}
 
-	console.log('Log 3');
 
-	xhr.open('PUT', "https://ws.xirsys.com/_ns/www.wimf.com/wimf-foglet/"+channelG, true);
+	xhr.open('PUT', "https://ws.xirsys.com/_ns/www.wimf.com/wimf-foglet/"+channel, true);
 	xhr.setRequestHeader ("Authorization", "Basic " + btoa("WhereIsMyFriends:3fea7eb8-d07b-11e7-be18-fd777e1dd627") );
 	xhr.send();
 
@@ -49,3 +44,6 @@ function join(nForm){
 	}
 }
 
+function generate(nform){
+	nForm.channel.value = guid();
+}
