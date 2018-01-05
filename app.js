f9284a5f30e8d2074fabcade5fc856dc56245640
PineauSullivan <pineau.sullivan@gmail.com>
2018-01-05 17:03:16 +0100

    Master: fix generate

--- app.js
+++ app.js
@@ -15,25 +15,21 @@ function create(nForm){
 
 	  	var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function($evt){
-		if(xhr.readyState == 4 && xhr.status == 200){
-		     var res = JSON.parse(xhr.responseText);
-		     console.log('response: ',res);
-		     if(res.s=="ok"){
-			    document.getElementById("buttonCreate").style.backgroundColor = "green";
-		     	document.location.href="app/index.html?"+channel;
-		     }else{
-			    document.getElementById("buttonCreate").style.backgroundColor = "red";
-		     }
+			if(xhr.readyState == 4 && xhr.status == 200){
+			     var res = JSON.parse(xhr.responseText);
+			     console.log('response: ',res);
+			     if(res.s=="ok"){
+				    document.getElementById("buttonCreate").style.backgroundColor = "green";
+			     	document.location.href="app/index.html?"+channel;
+			     }else{
+				    document.getElementById("buttonCreate").style.backgroundColor = "red";
+			     }
+			}
 		}
-
-
 		xhr.open('PUT', "https://ws.xirsys.com/_ns/www.wimf.com/wimf-foglet/"+channel, true);
 		xhr.setRequestHeader ("Authorization", "Basic " + btoa("WhereIsMyFriends:3fea7eb8-d07b-11e7-be18-fd777e1dd627") );
 		xhr.send();
-
-	  }
 	}
-
 }
 
 function join(nForm){
